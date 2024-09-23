a734961e76b772c28b3ffd9ca1ae8fbee29b1dd6
Sasha Panteleev <spanteleev838.com>
2024-09-23 13:15:46 +0500

    задачи 1 - 3

--- styles/ConsoleApp27/ConsoleApp27/Program.cs
+++ styles/ConsoleApp27/ConsoleApp27/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp27
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+            int number = 15;
+
+            Console.WriteLine($"Куб числа {number}: {Cube(number)}");
+            Console.WriteLine($"Квадрат числа {number}: {Square(number)}");
+            Console.WriteLine($"Сумма чисел {number} и 3: {Sum(number, 3)}");
+
+            Console.ReadKey();
+        }
+
+        
+        static int Cube(int x)
+        {
+            return x * x * x;
+        }
+
+    
+        static int Square(int x)
+        {
+            return x * x;
+        }
+
+      
+        static int Sum(int x, int y)
+        {
+            return x + y;
         }
     }
 }
